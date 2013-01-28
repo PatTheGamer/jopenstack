d094c950abf7f8df6a6a2b118facf5cc47fb72cf
Ryan Moore <ryan.moore@greyrocksoft.com>
2013-01-28 11:39:46 -0700

    Adding initial files for a gradle project for jopenstack.

---
+++ jopenstack/src/main/java/com/greyrocksoft/jopenstack/Main.java
@@ -0,0 +1,12 @@
+package com.greyrocksoft.jopenstack;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Main Start Method");
+	}
+
+}
